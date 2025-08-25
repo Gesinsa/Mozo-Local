532f47128644ede52c45f24e89fd7ff5f31f3b64
Gestores de Sistemas Informaticos SRL <gesinsasrl@gmail.com>
2025-08-25 11:32:32 -0400

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '465445fe7ff48a00fff3a86dcfb0a9f2922ec9c2',
+    'name' => 'saul/ventas-tablet',
+  ),
+  'versions' => 
+  array (
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.7.x-dev',
+      ),
+      'reference' => '994ecccd8f3283ecf5ac33254543eb0ac946d525',
+    ),
+    'saul/ventas-tablet' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '465445fe7ff48a00fff3a86dcfb0a9f2922ec9c2',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v4.2.0',
+      'version' => '4.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da64796370fc4eb03cc277088f6fede9fde88482',
+    ),
+  ),
+);
